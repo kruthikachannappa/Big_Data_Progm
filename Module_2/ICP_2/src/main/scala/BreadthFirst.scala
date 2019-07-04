31e90d97230926e10c1efecca6f3d80e611c39f5
kruthikachannappa <47044626+kruthikachannappa@users.noreply.github.com>
2019-07-03 21:22:26 -0400

    Update BreadthFirst.scala

--- Module_2/ICP_2/src/main/scala/BreadthFirst.scala
+++ Module_2/ICP_2/src/main/scala/BreadthFirst.scala
@@ -8,7 +8,6 @@ object BreadthFirst {
     type Vertex = Int
     type Graph = Map[Vertex, List[Vertex]]
     val g: Graph = Map(1 -> List(2,3,5,6,7), 2 -> List(1,3,4,6,7), 3 -> List(1,2), 4 -> List(2,5,7),5 -> List(1,6,7),6 -> List(1,2,5,7),7 -> List(1,2,4,5,6))
-   
 
     //I want this to return results in the different layers that it finds them (hence the list of list of vertex)
     def BFS(start: Vertex, g: Graph): List[List[Vertex]] = {
@@ -24,10 +23,25 @@ object BreadthFirst {
     }
 
 
-    val bfsresult=BFS(1,g )
 
+    def DFS(start: Vertex, g: Graph): List[Vertex] = {
+
+      def DFS0(v: Vertex, visited: List[Vertex]): List[Vertex] = {
+        if (visited.contains(v))
+          visited
+        else {
+          val neighbours:List[Vertex] = g(v) filterNot visited.contains
+          neighbours.foldLeft(v :: visited)((b,a) => DFS0(a,b))
+        }
+      }
+      DFS0(start,List()).reverse
+    }
+
+    val bfsresult=BFS(1,g)
+    val dfsresult=DFS(1,g )
     println(bfsresult.mkString(","))
-  
+   println(dfsresult.mkString(","))
+
   }
 
 
